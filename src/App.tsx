5c95fc93c5ac05c6b3cdc8aef37987ae9467fc00
salmariza <salmarizaululazmi@gmail.com>
2026-02-14 21:56:38 +0700

    feat(frontend):fetch data in RoomListPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RoomListPage from './pages/RoomListPage';  
 
 const App: React.FC = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  //const secretKey = process.env.local.REACT_APP_SECRET_KEY;
+
+  console.log(apiUrl); 
+  //console.log(secretKey); 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Selamat datang di sistem peminjaman ruangan!</h1>} />
+        <Route path="/" element={<h1>Selamat datang di sistem peminjaman ruangan! 
+          <a href="/rooms">Lihat Daftar Ruangan</a></h1>} />
         <Route path="/rooms" element={<RoomListPage />} />
       </Routes>
     </Router>
--- src/pages/RoomListPage.tsx
+++ src/pages/RoomListPage.tsx
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import api from '../api';
+import api from '../services/api';  
 
 const RoomListPage: React.FC = () => {
 const [rooms, setRooms] = useState<any[]>([]);
+const [loading, setLoading] = useState<boolean>(true);
+const [error, setError] = useState<string | null>(null);
 
-useEffect(() => {
+    useEffect(() => {
     api.get('/rooms')
-    .then(response => {
+        .then((response) => {
         setRooms(response.data);
+        setLoading(false);
     })
-    .catch(error => {
-        console.error('There was an error fetching the rooms!', error);
+    .catch((error) => {
+        setError('Failed to fetch rooms');
+        setLoading(false);
     });
 }, []);
 
-return (
+    if (loading) {
+    return <p>Loading...</p>; // Menampilkan loading state
+    }
+
+    if (error) {
+    return <p>{error}</p>; // Menampilkan error jika ada
+    }
+
+    return (
     <div>
         <h1>Daftar Ruangan</h1>
         {rooms.length > 0 ? (
         <ul>
             {rooms.map((room: any) => (
             <li key={room.id}>
-            {room.name} - {room.location} - Kapasitas: {room.capacity} - Status: {room.isActive ? 'Aktif' : 'Tidak Aktif'}
+                <h2>{room.name}</h2>
+                <p>Lokasi: {room.location}</p>
+                <p>Kapasitas: {room.capacity}</p>
+                <p>Status: {room.isActive ? 'Aktif' : 'Tidak Aktif'}</p>
             </li>
         ))}
         </ul>
@@ -29,7 +44,7 @@ return (
         <p>Belum ada ruangan yang tersedia.</p>
     )}
     </div>
-);
+    );
 };
 
 export default RoomListPage;
